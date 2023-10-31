bc7cbc54e4c3ab67151ec9008867af7df2affaac
Yitus <148155334+Yitus@users.noreply.github.com>
2023-10-31 18:11:23 -0500

    Update and rename Ahorcado.py to Ahorcado y buscaminas.py

---
+++ Ahorcado y buscaminas.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinten import messagebox
+import random 
+import time
+# se ponen mas cosas pero no se ni lo que son ni lo que significan :)
+Disbombas = 
+banderasbomba = 
+Lbomb = []
+# Faltan cosas pq pues estas son las variables y no tengo idea de cuantas variables vamos a poner XD
+bomb = 0
+bomb1 = 0
+bomb2 = 0
+bomb3 = 0 
+bombo4 = 0
+def Bombran ():
+  
