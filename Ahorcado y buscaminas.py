e7fb374e623e71c9f02005808499ec104c43ae95
Yitus <148155334+Yitus@users.noreply.github.com>
2023-11-11 16:57:29 -0500

    Update Ahorcado y buscaminas.py

--- Ahorcado y buscaminas.py
+++ Ahorcado y buscaminas.py
@@ -2,11 +2,53 @@ from tkinter import *
 from tkinten import messagebox
 import random 
 import time
+import pygame
 # se ponen mas cosas pero no se exactamente cuales
+
+
+#pygame, necesito revisar y ver todo
+self.fuente = pygame.font.SysFont('newyorkitalicttf', self.width, True)     #Numeros
+self.temp = pygame.font.SysFont('newyorkitalicttf', 20, True)               #Tiempo
+self.msg = pygame.font.SysFont('newyorkitalicttf', 30, True)                #Mensaje de victoria y derrota
+self.p_ancho = ancho
+self.p_alto = alto
+self.ventana = pygame.display.set_mode((self.p_ancho, self.p_alto))         #Ventana y dimensiones
+pygame.display.set_caption("Buscaminas")    
+self.centro = self.ventana.get_rect().center                                #Centro de la ventana
+mina = pygame.image.load("Mina.png")               
+self.mina = pygame.transform.scale(mina, (self.width , self.width))         #Imagen de la mina
+bandera = pygame.image.load("Bandera.png")
+self.bandera = pygame.transform.scale(bandera, (self.width, self.width))    #Imagen de la bandera
+pygame.mixer.init()                                                                     
+
+#buscaminas
 Disbombas = 
 banderasbomba = 
 Lbomb = []
+self.banderas_restantes = 0
+self.banderas_max = 0
+self.cuadricula = []
+self.Crcuadricula = []
+self.minas_d = {}
+self.minasA_dt = {}
+self.ColocarMinas()
+self.cont_band = 0
+
+def Crcuadricula =(self):
+  x = s
+
 # Faltan cosas pq pues estas son las variables y no tengo idea de cuantas variables vamos a poner XD
+
+ def tablero (filas,columnas,va 
+
+def bombasran (self):
+  self.bombasran = {}
+  self.Nbanderas = 0
+  self.Mbanderas = 0
+
+
+
+
 bomb = 0
 bomb1 = 0
 bomb2 = 0
