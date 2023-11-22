9c29b18ed56d61a80f1e4d708a71e28fc336eaf5
Alockw <149006455+Alockw@users.noreply.github.com>
2023-11-22 00:23:59 -0500

    menu1

---
+++ menu.py
@@ -0,0 +1,90 @@
+import pygame
+import sys
+
+# Inicializar Pygame
+pygame.init()
+
+# Configurar la pantalla
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Configurar colores
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+# Clase para los botones
+class Button:
+    def __init__(self, color, x, y, width, height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+
+    def draw(self, screen, outline=None):
+        if outline:
+            pygame.draw.rect(screen, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), 0)
+        if self.text != '':
+            font = pygame.font.SysFont('comicsans', 50)
+            text = font.render(self.text, 1, (0, 0, 0))
+            screen.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+    def is_over(self, pos):
+        if self.x < pos[0] < self.x + self.width:
+            if self.y < pos[1] < self.y + self.height:
+                return True
+        return False
+
+# Crear botones para los juegos y niveles de dificultad
+ahorcado_button = Button(white, 100, 100, 200, 50, 'Ahorcado')
+buscaminas_button = Button(white, 500, 100, 200, 50, 'Buscaminas')
+facil_button = Button(white, 100, 300, 200, 50, 'Facil')
+medio_button = Button(white, 300, 300, 200, 50, 'Medio')
+dificil_button = Button(white, 500, 300, 200, 50, 'Dificil')
+
+# Implementar los juegos de Ahorcado y Buscaminas
+def juego_ahorcado(dificultad):
+    # Aquí va el código para iniciar el juego de Ahorcado
+    print(f"Iniciando Ahorcado en nivel {dificultad}...")
+    pass
+
+def juego_buscaminas(dificultad):
+    # Aquí va el código para iniciar el juego de Buscaminas
+    print(f"Iniciando Buscaminas en nivel {dificultad}...")
+    pass
+
+# Bucle principal para manejar eventos y actualizar pantalla
+def game_loop():
+    while True:
+        for event in pygame.event.get():
+            pos = pygame.mouse.get_pos()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if ahorcado_button.is_over(pos):
+                    if facil_button.is_over(pos):
+                        juego_ahorcado('facil')
+                    elif medio_button.is_over(pos):
+                        juego_ahorcado('medio')
+                    elif dificil_button.is_over(pos):
+                        juego_ahorcado('dificil')
+                if buscaminas_button.is_over(pos):
+                    if facil_button.is_over(pos):
+                        juego_buscaminas('facil')
+                    elif medio_button.is_over(pos):
+                        juego_buscaminas('medio')
+                    elif dificil_button.is_over(pos):
+                        juego_buscaminas('dificil')
+
+        screen.fill(black)
+        ahorcado_button.draw(screen)
+        buscaminas_button.draw(screen)
+        facil_button.draw(screen)
+        medio_button.draw(screen)
+        dificil_button.draw(screen)
+        pygame.display.update()
+
+game_loop()
