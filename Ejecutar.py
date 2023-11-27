e0450ee1581051c761aee4aa9317bdbdfb082b31
Yitus <148155334+Yitus@users.noreply.github.com>
2023-11-27 10:03:42 -0500

    Update Ejecutar.py

--- Ejecutar.py
+++ Ejecutar.py
@@ -11,4 +11,16 @@ pos_arriba = (juego.centro[0] - 75, juego.centro[1] - (juego.grosor + 5) * (jueg
 pos_abajo = (juego.centro[0], juego.centro[1] + (juego.grosor + 5) * (juego.ancho/2) + 15)
 run = True
 while run:
-  game.
+  game.Mostrarcuad()                                                                   
+    event = pygame.event.poll()                                                                                     #Lee los eventos
+    if event.type == pygame.QUIT:   
+        run = False                                                                                                 #Termina el ciclo
+    if event.type == pygame.MOUSEBUTTONDOWN and juega:  
+        pos = event.dict['pos']                                                                                     #Lee la posicion del mouse
+        if event.button == 1:   
+            if x == 0:  
+                x = juego.safe(pos)                                                                                 #Ejecuta el metodo safe, si se ejecuta adecuadamente retorna 1 y no se vuelve a hacer
+            game.ejecucion(pos)
+        if event.button == 3:
+            juego.Poner(pos)
+  
