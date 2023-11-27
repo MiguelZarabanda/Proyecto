a6c5b43b2f238366e33fc80af27fed6db2aea4de
Yitus <148155334+Yitus@users.noreply.github.com>
2023-11-27 09:53:48 -0500

    Ejecutar.py

---
+++ Ejecutar.py
@@ -0,0 +1,14 @@
+import pygame 
+import time 
+import definicion as Df
+
+pygame.init()
+x = 0
+game = DF.game(700,600)
+t1 = time.perf_counter()
+juega = True
+pos_arriba = (juego.centro[0] - 75, juego.centro[1] - (juego.grosor + 5) * (juego.ancho/2) - 40)
+pos_abajo = (juego.centro[0], juego.centro[1] + (juego.grosor + 5) * (juego.ancho/2) + 15)
+run = True
+while run:
+  game.
